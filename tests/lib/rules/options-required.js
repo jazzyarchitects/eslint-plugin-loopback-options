17551b2994ba5efdbd5373bbe4cf39b9ef0450e3
Jibin Mathews <jibinmathews7@gmail.com>
2017-11-08 16:38:20 +0530

    Moving to CallExpression from ExpressionStatement

--- tests/lib/rules/options-required.js
+++ tests/lib/rules/options-required.js
@@ -15,17 +15,50 @@ var rule = require('../../../lib/rules/options-required'),
 // Tests
 //------------------------------------------------------------------------------
 
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+});
+
 var ruleTester = new RuleTester();
+
 ruleTester.run('options-required', rule, {
-  valid: ['ticket.save(options)', 'Model.create({}, options)', 'Model.create(objName, options)'],
+  valid: [
+    'ticket.save(options)',
+    'Model.create({}, options)',
+    'Model.create(objName, options)',
+    'async () => { await ticket.save(options) }',
+    'async () => { await Model.create({}, options)}',
+    'async () => { await Model.create(objName, options) }',
+  ],
 
   invalid: [
+    {
+      code: 'async () => { await ticket.save() }',
+      errors: [
+        {
+          message: 'Options should be passed to save() function',
+          type: 'CallExpression',
+        },
+      ],
+    },
+    {
+      code: 'async () => { await Model.create(modelObj) }',
+      errors: [
+        {
+          message: "'options' should be the last argument passed to create() function",
+          type: 'CallExpression',
+        },
+      ],
+    },
     {
       code: 'ticket.save()',
       errors: [
         {
           message: 'Options should be passed to save() function',
-          type: 'ExpressionStatement',
+          type: 'CallExpression',
         },
       ],
     },
@@ -34,7 +67,7 @@ ruleTester.run('options-required', rule, {
       errors: [
         {
           message: "'options' should be the last argument passed to create() function",
-          type: 'ExpressionStatement',
+          type: 'CallExpression',
         },
       ],
     },
